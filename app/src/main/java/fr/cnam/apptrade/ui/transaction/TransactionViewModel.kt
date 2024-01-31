b2508688486fedde927e55257ea12720b04c5014
llarboulletmarin <l.larboulletmarin@gmail.com>
2024-01-31 20:15:48 +0100

    update layout transaction

--- app/src/main/java/fr/cnam/apptrade/ui/transaction/TransactionViewModel.kt
+++ app/src/main/java/fr/cnam/apptrade/ui/transaction/TransactionViewModel.kt
@@ -8,11 +8,13 @@ import fr.cnam.apptrade.network.models.Currency
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Call
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 
 class TransactionViewModel : ViewModel() {
 
-    private val _currencyData = MutableLiveData<List<Currency>>()
-    val currencyData: LiveData<List<Currency>> = _currencyData
+    private val _currencyData = MutableLiveData<Currency>()
+    val currencyData: LiveData<Currency> = _currencyData
 
     fun fetchCurrency(currencyCode: String) {
         ApiClient.currencyApiService.getCurrency(currencyCode).enqueue(object : Callback<List<Currency>> {
@@ -23,14 +25,17 @@ class TransactionViewModel : ViewModel() {
                     println("[DEBUG] Response: $response")
                     if (response.isSuccessful) {
                         println("[DEBUG] Currency: ${response.body()}")
-                        _currencyData.postValue(response.body())
+                        val currency = response.body()?.get(0)
+                        currency?.price = currency?.price?.setScale(4, 2)!!
+                        _currencyData.postValue(currency!!)
+
                     } else {
-                        _currencyData.postValue(emptyList())
+                        _currencyData.postValue(Currency("", "", 0.toBigDecimal()))
                     }
                 }
                 override fun onFailure(call: Call<List<Currency>>, t: Throwable) {
                    println("[DEBUG] Error: $t")
-                    _currencyData.postValue(emptyList())
+                    _currencyData.postValue(Currency("", "", 0.toBigDecimal()))
                 }
             })
     }
